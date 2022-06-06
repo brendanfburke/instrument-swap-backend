5e8564c7a4bd7e484d9c19a5ede131527e757442
brendanburke <bfburke33@gmail.com>
2022-06-06 11:38:10 -0400

    set up user and listing models, working on post routes

---
+++ models/Listing.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+const listingSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: [true, 'Enter  a title'],
+        unique: true
+    },
+    description: {
+        type: String,
+        required: [true, 'Enter a description']
+    },
+    image: {
+        type: String,
+    }
+}, { timestamps: true })
+
+const Listing = mongoose.model('Listing', listingSchema);
+
+module.exports = Listing;
--- server.js
+++ server.js
@@ -4,11 +4,26 @@ const { PORT, MONGODB_URI } = process.env
 
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require("cors");
+const morgan = require("morgan");
+
+
+const User = require('./models/User');
 
 const app = express()
 
-
 require('./config/db.connection')
+
+app.post('/users', async (req, res, next) => {
+    try {
+        res.json(await User.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+
+
+
 
 
 app.get('/', (req, res) => {
