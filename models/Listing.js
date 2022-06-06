387ee36d79e3e6e9c1a99133f4be8e8af53007ff
brendanburke <bfburke33@gmail.com>
2022-06-06 11:59:03 -0400

    user and listing models working, researching aws s3 image hosting for the listing model

--- models/Listing.js
+++ models/Listing.js
@@ -17,4 +17,5 @@ const listingSchema = new mongoose.Schema({
 
 const Listing = mongoose.model('Listing', listingSchema);
 
+
 module.exports = Listing;
--- server.js
+++ server.js
@@ -7,16 +7,37 @@ const mongoose = require('mongoose')
 const cors = require("cors");
 const morgan = require("morgan");
 
-
+const db = require("./models")
 const User = require('./models/User');
 
 const app = express()
 
 require('./config/db.connection')
 
+app.get('/users', async (req, res, next) => {
+    try {
+        res.json(await db.User.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
 app.post('/users', async (req, res, next) => {
     try {
-        res.json(await User.create(req.body))
+        res.json(await db.User.create(req.body))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+app.get('/listings', async (req, res, next) => {
+    try {
+        res.json(await db.Listing.find({}))
+    } catch (error) {
+        res.status(400).json(error)
+    }
+})
+app.post('/listings', async (req, res, next) => {
+    try {
+        res.json(await db.Listing.create(req.body))
     } catch (error) {
         res.status(400).json(error)
     }
