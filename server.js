1acc28b187c2217ea2e2e6ae74eff9f7a5dc514d
brendanburke <bfburke33@gmail.com>
2022-06-07 10:20:21 -0400

    photo upload working on backend, time to build a basic frontend to test user upload

--- server.js
+++ server.js
@@ -6,9 +6,16 @@ const express = require('express')
 const mongoose = require('mongoose')
 const cors = require("cors");
 const morgan = require("morgan");
+const upload = require('./common')
 
 const db = require("./models")
-const User = require('./models/User');
+
+const { uploadFile } = require('./s3')
+
+const fs = require('fs')
+const util  = require('util')
+const unlinkFile = util.promisify(fs.unlink)
+
 
 const app = express()
 
@@ -42,6 +49,22 @@ app.post('/listings', async (req, res, next) => {
         res.status(400).json(error)
     }
 })
+
+app.post("/single", upload.single("image"), async (req, res) => {
+    console.log(req.file);
+    // uploading to AWS S3
+    const result = await uploadFile(req.file);  // Calling above function in s3.js
+    console.log("S3 response", result);
+    // You may apply filter, resize image before sending to client
+    // Deleting from local if uploaded in S3 bucket
+    await unlinkFile(req.file.path);
+    res.send({
+      status: "success",
+      message: "File uploaded successfully",
+      data: req.file,
+    });
+  });
+  
 
 
 
