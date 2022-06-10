4e70329aadc94b0e2a68654e6d033b10a1a80fa5
brendanburke <bfburke33@gmail.com>
2022-06-10 15:06:44 -0400

    working aws issue

--- server.js
+++ server.js
@@ -75,19 +75,18 @@ app.get('/users', async (req, res, next) => {
 
 
 app.post("/single", upload.single("image"), async (req, res) => {
-    // console.log(req.file.filename);
-    // // uploading to AWS S3
-    // const result = await uploadFile(req.file);  // Calling above function in s3.js
-    // console.log("S3 response", result);
-    // // You may apply filter, resize image before sending to client
-    // // Deleting from local if uploaded in S3 bucket
-    // await unlinkFile(req.file.path);
-    // res.send({
-    //   status: "success",
-    //   message: "File uploaded successfully",
-    //   data: req.file,
-    // });
-    res.send('hi')
+    console.log(req.file.filename);
+    // uploading to AWS S3
+    const result = await uploadFile(req.file);  // Calling above function in s3.js
+    console.log("S3 response", result);
+    // You may apply filter, resize image before sending to client
+    // Deleting from local if uploaded in S3 bucket
+    await unlinkFile(req.file.path);
+    res.send({
+      status: "success",
+      message: "File uploaded successfully",
+      data: req.file,
+    });
   });
 
   app.get('/single', (req, res) => {
